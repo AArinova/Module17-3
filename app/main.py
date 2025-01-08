7b3650712b99cac9203e1b49be0cc8ddd28fb701
AArinova <aliya-arinova@yandex.ru>
2025-01-08 19:18:18 +0700

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from routers import task, user
+from
+
+app = FastAPI()
+
+@app.get('/')
+async def welcome():
+    return {'message': 'Welcome to TaskManager'}
+
+app.include_router(task.router)
+app.include_router(user.router)
